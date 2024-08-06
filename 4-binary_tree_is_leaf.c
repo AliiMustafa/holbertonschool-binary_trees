aba2fdaf8922abfc27510f6f2a2bc5b7c861925f
AliiMustafa <aliimustafazada@gmail.com>
2024-08-06 18:28:08 +0400

    is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - function
- * @tree: pointer
- * Return: Nothing
+ * binary_tree_is_leaf - function
+ * @node: pointer
+ * Return: 0 or 1
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
