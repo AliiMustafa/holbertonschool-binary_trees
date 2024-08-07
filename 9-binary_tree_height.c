6e2ab50e6ffdd6376404f6689868f302613ce7ec
AliiMustafa <aliimustafazada.com>
2024-08-07 14:38:46 +0400

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left || tree->right)
 	{
 		return (binary_tree_height(tree->left) + 1);
-		binary_tree_height(tree->right);
+		return (binary_tree_height(tree->right) + 1);
 	}
 	else
 		return (0);
