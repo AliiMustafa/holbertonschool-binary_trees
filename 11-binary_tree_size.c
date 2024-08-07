df61964adf2ee499d939e049dedc000ca5d22fed
AliiMustafa <aliimustafazada.com>
2024-08-07 15:59:05 +0400

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function
+ * @tree: pointer
+ * Return: 0 or size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (tree == NULL)
+		return (0);
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+	return (l + r + 1);
+	
+}
+
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,5 +22,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (l);
 	else
 		return (r);
+	
 }
 
