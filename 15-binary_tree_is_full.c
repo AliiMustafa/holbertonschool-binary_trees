64829029df094761d0b90eb5b54b76f55121d234
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-07 13:41:05 +0000

    Created file

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,23 +1,37 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_full - function
- * @tree: pointer
- * Return: 0 or number of nodes with at leats 1 child in tree
+ * binary_tree_is_full - function that says if a tree is full or not
+ * a tree is full if it has tow or none children
+ * @tree: tree to check
+ * Return: 1 if it is a full 0 if not
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int l, r;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
-		return (0);
-	if ()
 	{
-		binary_tree_is_full(tree->left);
-		binary_tree_is_full(tree->right);
+		return (0);
 	}
 	else
-		return (0);
-	return (l + r + 1);
+	{
+		if (tree->left && tree->right)
+		{
+			left = binary_tree_is_full(tree->left);
+			right = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		else if (!tree->left && !tree->right)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
 }
-
