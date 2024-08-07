dac49aa766248cf7d992853146e1510a06636f3d
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-07 15:08:16 +0000

    Simplification

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,10 +1,15 @@
 #include "binary_trees.h"
+#include <stdio.h>
+
 
 /**
  * binary_tree_is_leaf - function
  * @node: pointer
  * Return: 0 or 1
+ *
  */
+
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
