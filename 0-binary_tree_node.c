8f07d5674bcbf3f51e7a729f77eff93ce2c0fbff
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-07 14:23:22 +0000

    Rename nodename

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,26 +1,28 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_node - function
  * @parent: pointer
  * @value: variable
  * Return: a pointer to the new node or NULL
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *newnode;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
 	{
 		return (0);
 	}
 	if (parent == NULL)
 	{
-		new->parent = NULL;
+		newnode->parent = NULL;
 	}
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-	new->n = value;
-	return (new);
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	newnode->n = value;
+	return (newnode);
 }
